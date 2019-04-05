b623382e4d7c1781cc364aae53757c79929d74c8
Naomie <naomie.colombo@gmail.com>
2019-04-05 18:13:41 +0200

    add new file

---
+++ test.py
@@ -0,0 +1,29 @@
+import requests
+
+class API: 
+	def __init__(self): 
+		self.url_1 = 'https://world.openfoodfacts.org/cgi/search.pl'
+		self.param = {
+		'action':'process',
+		'tagtype_0':'categories',
+		'tag_contains_0':'contains',
+		'tag_0':'boisson',
+		'page_size':20,
+		'json':1
+		}
+
+	def request(self, tag):
+		self.param["tag_0"] = tag
+		r = requests.get(self.url_1, params=self.param)
+		result = r.json()
+		for val in result["products"]:
+			print("Nom:", val["product_name_fr"], "note nutritionnelle:", val["nutrition_grades"])
+		print(r.url)
+
+
+test = API()
+
+test.request("chips")
+
+
+
