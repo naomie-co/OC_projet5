9cd3171e25cca00d81e66849469bff5c2af64cf2
Naomie <naomie.colombo@gmail.com>
2019-05-02 20:43:45 +0200

    creation of a data base's class

--- classes.py
+++ classes.py
@@ -1,5 +1,7 @@
 import requests
 import pymysql
+import DB_info
+#import db_informations
 
 class API: 
 	def __init__(self): 
@@ -9,27 +11,102 @@ class API:
 		'tagtype_0':'categories',
 		'tag_contains_0':'contains',
 		'tag_0':'boisson',
-		'page_size':20,
+		'page_size':35,
 		'json':1
 		}
 
-	def request(self, tag):
+	def request_product(self, tag):
+		"""get products from the API"""
+		i = 0
 		self.param["tag_0"] = tag
 		r = requests.get(self.url_1, params=self.param)
 		result = r.json()
-		for val in result["products"]:
-			print("Nom:", val["product_name_fr"], "note nutritionnelle:", val["nutrition_grades"])
-		print(r.url)
-		print(result)
+		data = []
+		while i < 20:
+			for val in result["products"]:
+				try:
+					data.append([val["product_name_fr"], val["nutrition_grades"]])
+				except KeyError:
+					pass
+				i += 1
+		print(data)
+		return data
+
+class DB:
+	def __init__(self):
+		self.host = DB_info.HOST
+		self.user = DB_info.USER
+		self.password = DB_info.PASSWORD
+		self.db = DB_info.DB
+		self.connection1 = pymysql.connect(host=self.host, user=self.user, password=self.password)
+		self.connection2 = pymysql.connect(host=self.host, user=self.user, password=self.password, db=self.db)
+
+	def createDB(self):
+		with self.connection1.cursor() as cursor:
+			sql = "CREATE DATABASE IF NOT EXISTS off"
+			cursor.execute(sql)
+		self.connection.commit()
+
+	def showDB(self):
+		with self.connection1.cursor() as cursor:
+			sql = "SHOW DATABASES;"
+			cursor.execute(sql)
+			result = cursor.fetchall()
+			print(result)
+
+	def createTableOpfood(self):
+		with self.connection2.cursor() as cursor:
+			sql = """CREATE TABLE IF NOT EXISTS opfood (
+			id int unsigned NOT NULL AUTO_INCREMENT,
+			nom varchar(200),
+			nutriscore varchar(1),
+			categorie varchar (200),
+			url varchar(500), 
+			PRIMARY KEY (id))
+			"""
+			cursor.execute(sql)
+		self.connection2.commit()
+
+	def describeTable(self):
+		with self.connection2.cursor() as cursor:
+			sql = "DESCRIBE opfood;"
+			cursor.execute(sql)
+			result = cursor.fetchall()
+			print(result)
+
+	def dropOpfoodTable(self):
+		with self.connection2.cursor() as cursor:
+			sql = "DROP TABLE IF EXISTS opfood"
+			cursor.execute(sql)
+		self.connection2.commit()
+
+	def insert(self):
+		r = API()
+		r = r.request_product("boisson")
+		with self.connection2.cursor() as cursor:
+			for value in r:
+				sql = "INSERT INTO opfood (nom, nutriscore) VALUES ('%s', '%s')" % (value[0], value[1])
+				cursor.execute(sql)
+		self.connection2.commit()
+
+	def showTable(self):
+		with self.connection2.cursor() as cursor:
+			sql = "SELECT * FROM opfood"
+			cursor.execute(sql)
+			print(cursor.fetchall())
+
+class display:
+	
 
 
-#class DataBase(self):
-#	def __init__(self):
+r = API()
 
-
-test = API()
-
-test.request("chips")
-
-
-
+r.request_product("farine")
+db1 = DB()
+#db1.createDB()
+db1.showDB()
+db1.dropOpfoodTable()
+db1.createTableOpfood()
+db1.describeTable()
+db1.insert()
+db1.showTable()
